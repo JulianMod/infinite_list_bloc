513a381046ff5badd656fe84bd7adcd4e0f14eb3
JuliaMod <julmodlinski@gmail.com>
2022-11-14 16:03:30 +0100

    add event transformer

--- lib/posts/bloc/post_bloc.dart
+++ lib/posts/bloc/post_bloc.dart
@@ -1,16 +1,27 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:infinite_list_bloc/bloc/bloc.dart';
 import 'package:infinite_list_bloc/post.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
+
 class PostBloc extends Bloc<PostEvent, PostState> {
   PostBloc({required this.httpClient}) : super(const PostState()) {
-    on<PostFetched>(_onPostFetched);
+    on<PostFetched>(_onPostFetched,
+    transformer: throttleDroppable(throttleDuration));
   }
 
   Future<void> _onPostFetched(PostFetched event, Emitter<PostState> emit) async {
